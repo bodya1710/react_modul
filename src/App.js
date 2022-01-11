a149e28aa73c5573193eb802ba958510d9142f22
bodya1710 <bodyabordiyk@gmail.com>
2022-01-11 20:24:26 +0200

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Users from "./components/Users/Users";
+import Posts from "./components/Posts/Posts";
+import Comments from "./components/Comments/Comments";
+import './components/style/style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'container'}>
+        <div className={'block_users_posts'}>
+            <Users/>
+            <Posts/>
+        </div>
+        <div className={'block_coments'}><Comments/></div>
     </div>
   );
 }
---
+++ src/components/User/User.js
@@ -0,0 +1,13 @@
+
+function User(props) {
+    const {id, name, username} = props;
+    return (
+        <div className={'user'}>
+            <h2>Id - {id}</h2>
+            <p>Name - {name}</p>
+            <p>Surname - {username}</p>
+        </div>
+    );
+}
+
+export default User;
